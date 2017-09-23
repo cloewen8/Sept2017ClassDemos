9bad912565d2317a0b06c1c93c9e4dcf544c479c
Colton Loewen <loewencjay@gmail.com>
2017-09-22 20:34:18 -0600

    Added BLL list methods

---
+++ ClassDemo/ChinookSystem/BLL/AlbumController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Chinook.Data.Entities;
+using ChinookSystem.DAL;
+using System.ComponentModel;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    public class AlbumController
+    {
+        
+    }
+}
---
+++ ClassDemo/ChinookSystem/BLL/ArtistController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Chinook.Data.Entities;
+using ChinookSystem.DAL;
+using System.ComponentModel;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class ArtistController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, true)]
+        public List<Artist> List()
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Artists.ToList();
+            }
+        }
+    }
+}
