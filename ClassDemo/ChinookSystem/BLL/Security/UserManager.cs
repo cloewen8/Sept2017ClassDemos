0ab4fc9c5f365a7e61161fb21920a4028fbffa10
Colton Loewen <loewencjay@gmail.com>
2017-10-25 19:43:49 -0600

    Moved security classes (refracting)

---
+++ ClassDemo/ChinookSystem/BLL/Security/UserManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Identity
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Chinook.Data.Entities.Security;
+using ChinookSystem.DAL.Security;
+#endregion
+
+namespace ChinookSystem.BLL.Security
+{
+    public class UserManager : UserManager<ApplicationUser>
+    {
+        public UserManager()
+            : base(new UserStore<ApplicationUser>(new ApplicationDbContext()))
+        {
+        }
+    }
+}
